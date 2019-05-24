ce377ef9eccb8999d6ae16ee4dd5d715731392f5
shubham1008042 <46986804+shubham1008042@users.noreply.github.com>
2019-05-24 11:07:44 +0530

    Update task.py

--- task.py
+++ task.py
@@ -1,9 +1,15 @@
+#"to print table of given no."
+
 n=11
 x=2
 for i in range(1,n):
     print("|",x ,"*", i,"|",x*i,"|")
 
 print("....................................")
+
+#Write a function called fizz_buzz that takes a number.If the number is divisible by 3, it should return “Fizz”.
+#If it is divisible by 5, it should return “Buzz”.If it is divisible by both 3 and 5, it should return “FizzBuzz”.
+#Otherwise, it should return the same number
 
 def fizzbuzz(num):
     if(num%3==0 and num%5==0):
@@ -18,6 +24,8 @@ fizzbuzz(12)
 fizzbuzz(21)
 
 print("......................................")
+
+#To find leap year
 
 def leap(year):
     if(year%4==0):
